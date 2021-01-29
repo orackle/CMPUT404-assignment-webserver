789305315413247bfaa18d2e9607fcc9468c54bc
orackle <debangan@ualberta.ca>
2021-01-28 23:09:13 -0700

    html for errors :]

--- server.py
+++ server.py
@@ -119,12 +119,12 @@ class MyWebServer(socketserver.BaseRequestHandler):
         page = """
         <!DOCTYPE html>
         <html>
-        <head>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        </head>
-        <body>
-            <h1>{}</h1>
-        </body>
+            <head>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            </head>
+            <body>
+                <h1 class="text-muted text-center">{}</h1>
+            </body>
         </html>
         """.format(status_code)
         return page
